2391949e86794754b4019884e698e80bb845ea78
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2024-02-07 08:34:34 -0300

    Adicinei Cappuccino a lista de receitas

--- api.py
+++ api.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 receitas_cafes = [
     {"nome": "Expresso", "ingredientes": ["água", "pó de café"], "instrucoes": "Use máquina de café expresso."},
     {"nome": "Café Americano", "ingredientes": ["água", "pó de café"], "instrucoes": "Misture água quente com café expresso."},
-    # Adicione mais 8 receitas aqui
+    {"nome": "Cappuccino", "ingredientes": ["água', "pó de café", "chocolate em pó", "creme de leite" "leite", "açucar"], "instruções": "Misture água quente com o pó de café e açucar, adicione leite quente, creme de leite e o chocolate em pó."},
+    # Adicione mais 7 receitas aqui
 ]
 
 # Rota para listar todas as receitas de cafés
